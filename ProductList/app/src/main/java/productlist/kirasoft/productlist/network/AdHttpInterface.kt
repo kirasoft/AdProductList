099d556aef5d422f46ca53899f17c01254462b11
Justin <megatenkota2@gmail.com>
2017-10-14 01:59:12 -0400

    query endpoint properly

--- ProductList/app/src/main/java/productlist/kirasoft/productlist/network/AdHttpInterface.kt
+++ ProductList/app/src/main/java/productlist/kirasoft/productlist/network/AdHttpInterface.kt
@@ -10,10 +10,7 @@ import retrofit2.http.GET
  */
 interface AdHttpInterface {
 
-
-    //http://ads.appia.com/getAds?id=236&amp;password=OVUJ1DJN&amp;siteId=4288&amp;deviceId=4230&amp;sessionId=techtestsession&amp;totalCampaignsRequested=10
-
-    @GET("getAds?id=236&password=OVUJ1DJN&siteId=4288&deviceId=4230&sessionId=techtestsession&totalCampaignsRequested=10&lname=Justin")
-    fun adStream(): Call<Ads>
+    @GET("getAds")
+    fun adStream(@Query id: String, @Query password: String, @Query deviceId: String): Call<Ads>
 
 }
